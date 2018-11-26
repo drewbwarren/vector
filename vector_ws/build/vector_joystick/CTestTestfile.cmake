02e9a20a92972776c4491a8a69cf4bc51217f4b8
Drew Warren <drewbwarren@gmail.com>
2018-11-25 19:55:28 -0700

    Initialize vector_ws for ros

---
+++ vector_ws/build/vector_joystick/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/drewbwarren/Documents/fun/vector/vector_ws/src/vector_joystick
+# Build directory: /home/drewbwarren/Documents/fun/vector/vector_ws/build/vector_joystick
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
